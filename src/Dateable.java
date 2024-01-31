064bd53b27aadb5156abd10dec5b8d073fb49609
mark <markt2026@student.cis.edu.hk>
2024-01-31 11:15:45 +0800

    Age and page

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class Dateable {
 
-    public static void main(String[] args) {
-        
-    }
+   public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scan.nextInt();
+        int dateable = (age/2 + 7);
+        System.out.println("If you are " + age + " you must date someone at least " + dateable + " years old");
+
+       int pages = (100 - age);
+       System.out.println("If you are " + age + " you must read at least " + pages + " pages a day");;
+   }
 }
